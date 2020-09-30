d43e13805330291b12d74e2e555fc6505d58ec96
Ruben Angeles <teckever6@hotmail.com>
2020-09-30 18:18:17 -0500

    Se Inicia Proyecto

---
+++ src/main/java/com/rubaanxd/inventarioJava/main/Main.java
@@ -0,0 +1,11 @@
+package com.rubaanxd.inventarioJava.main;
+
+/**
+ *
+ * @author rubaanxd
+ */
+public class Main {
+    public static void main(String[] args) {
+        com.rubaanxd.inventarioJava.ventanas.VentanaPrincipal.main(args);
+    }
+}
---
+++ src/main/java/com/rubaanxd/inventarioJava/misc/Computadora.java
@@ -0,0 +1,307 @@
+package com.rubaanxd.inventarioJava.misc;
+
+/**
+ *
+ * @author rubaanxd
+ */
+public class Computadora {
+    
+    private String serie;
+    private String nombre;
+    private String marca;
+    private String modelo;
+    private String mac;
+    private String ip;
+    private String cpu;
+    private String ram;
+    private String storage;
+    private String os;
+    private String STmarca;
+    private String STmodelo;
+    private String STcapacidad;
+    private String STrpm;
+    private String NETdhcp;
+    private String RAMinstalada;
+    private String RAMtipo;
+    private String RAMmaxima;
+
+    public Computadora() {
+    }
+
+    public Computadora(String serie, String nombre, String marca, String modelo, String mac, String ip, String cpu, String ram, String storage, String os, String STmarca, String STmodelo, String STcapacidad, String STrpm, String NETdhcp, String RAMinstalada, String RAMtipo, String RAMmaxima) {
+        this.serie = serie;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.mac = mac;
+        this.ip = ip;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+        this.os = os;
+        this.STmarca = STmarca;
+        this.STmodelo = STmodelo;
+        this.STcapacidad = STcapacidad;
+        this.STrpm = STrpm;
+        this.NETdhcp = NETdhcp;
+        this.RAMinstalada = RAMinstalada;
+        this.RAMtipo = RAMtipo;
+        this.RAMmaxima = RAMmaxima;
+    }
+
+    
+    
+    
+    /**
+     * @return the serie
+     */
+    public String getSerie() {
+        return serie;
+    }
+
+    /**
+     * @param serie the serie to set
+     */
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the mac
+     */
+    public String getMac() {
+        return mac;
+    }
+
+    /**
+     * @param mac the mac to set
+     */
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    /**
+     * @return the ip
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * @param ip the ip to set
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    /**
+     * @return the cpu
+     */
+    public String getCpu() {
+        return cpu;
+    }
+
+    /**
+     * @param cpu the cpu to set
+     */
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    /**
+     * @return the ram
+     */
+    public String getRam() {
+        return ram;
+    }
+
+    /**
+     * @param ram the ram to set
+     */
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    /**
+     * @return the storage
+     */
+    public String getStorage() {
+        return storage;
+    }
+
+    /**
+     * @param storage the storage to set
+     */
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    /**
+     * @return the os
+     */
+    public String getOs() {
+        return os;
+    }
+
+    /**
+     * @param os the os to set
+     */
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    /**
+     * @return the STmarca
+     */
+    public String getSTmarca() {
+        return STmarca;
+    }
+
+    /**
+     * @param STmarca the STmarca to set
+     */
+    public void setSTmarca(String STmarca) {
+        this.STmarca = STmarca;
+    }
+
+    /**
+     * @return the STmodelo
+     */
+    public String getSTmodelo() {
+        return STmodelo;
+    }
+
+    /**
+     * @param STmodelo the STmodelo to set
+     */
+    public void setSTmodelo(String STmodelo) {
+        this.STmodelo = STmodelo;
+    }
+
+    /**
+     * @return the STcapacidad
+     */
+    public String getSTcapacidad() {
+        return STcapacidad;
+    }
+
+    /**
+     * @param STcapacidad the STcapacidad to set
+     */
+    public void setSTcapacidad(String STcapacidad) {
+        this.STcapacidad = STcapacidad;
+    }
+
+    /**
+     * @return the STrpm
+     */
+    public String getSTrpm() {
+        return STrpm;
+    }
+
+    /**
+     * @param STrpm the STrpm to set
+     */
+    public void setSTrpm(String STrpm) {
+        this.STrpm = STrpm;
+    }
+
+    /**
+     * @return the NETdhcp
+     */
+    public String getNETdhcp() {
+        return NETdhcp;
+    }
+
+    /**
+     * @param NETdhcp the NETdhcp to set
+     */
+    public void setNETdhcp(String NETdhcp) {
+        this.NETdhcp = NETdhcp;
+    }
+
+    /**
+     * @return the RAMinstalada
+     */
+    public String getRAMinstalada() {
+        return RAMinstalada;
+    }
+
+    /**
+     * @param RAMinstalada the RAMinstalada to set
+     */
+    public void setRAMinstalada(String RAMinstalada) {
+        this.RAMinstalada = RAMinstalada;
+    }
+
+    /**
+     * @return the RAMtipo
+     */
+    public String getRAMtipo() {
+        return RAMtipo;
+    }
+
+    /**
+     * @param RAMtipo the RAMtipo to set
+     */
+    public void setRAMtipo(String RAMtipo) {
+        this.RAMtipo = RAMtipo;
+    }
+
+    /**
+     * @return the RAMmaxima
+     */
+    public String getRAMmaxima() {
+        return RAMmaxima;
+    }
+
+    /**
+     * @param RAMmaxima the RAMmaxima to set
+     */
+    public void setRAMmaxima(String RAMmaxima) {
+        this.RAMmaxima = RAMmaxima;
+    }
+    
+}
